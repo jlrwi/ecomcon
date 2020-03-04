cb29af4f116b847a109412cca7787933662eca49
Jonathan Reimer <wildreim@gmail.com>
2020-03-04 14:18:07 -0600

    packaged

--- ecomcon_curried.js
+++
@@ -1,7 +0,0 @@
-import ecomcon from "./ecomcon.js";
-
-export default Object.freeze(function (config) {
-    return function (input) {
-        return ecomcon(input, config.tags, config.comments, config.erase);
-    };
-});
